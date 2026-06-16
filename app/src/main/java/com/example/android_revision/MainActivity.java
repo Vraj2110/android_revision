1732be4802336e95ca222568409dd1cbc0d71aa5
Vraj Desai <desaivraj18@gmail.com>
2026-06-16 12:32:44 +0530

    new mini project task logger

--- app/src/main/java/com/example/android_revision/MainActivity.java
+++ app/src/main/java/com/example/android_revision/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android_revision;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
@@ -12,48 +14,14 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "LifeCycle";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG,"OnCreated called");
-    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(TAG,"OnStarted called");
-    }
+        EditText taskinput  = findViewById(R.id.etext1);
+        Button saveButton = findViewById(R.id.btn1);
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG,"OnResume called");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG,"onPaused called");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(TAG,"OnStopped called");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d(TAG,"OnRestarted called");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG,"OnDestroyed called");
     }
 }
